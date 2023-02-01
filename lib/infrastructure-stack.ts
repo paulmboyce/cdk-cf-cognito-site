bb4185b954339b286e3eaad4985a7f2cf1b4bdd6
paulmboyce <paul.boyce@goobito.com>
2023-02-01 15:40:39 +0000

    build(init cdk app): add the paxckage lock file

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3, aws_s3_deployment as s3Deployment } from "aws-cdk-lib";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const myBucket = new s3.Bucket(
+      this,
+      "com-bragaboo-web-cognito-app-bucket",
+      {
+        publicReadAccess: true,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        websiteIndexDocument: "index.html",
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const deployment = new s3Deployment.BucketDeployment(
+      this,
+      "deployStaticWebsite",
+      {
+        sources: [s3Deployment.Source.asset("../website")],
+        destinationBucket: myBucket,
+      }
+    );
   }
 }
