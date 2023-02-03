52fbec384812fde3101d172a026732f12dd875a4
paulmboyce <paul.boyce@goobito.com>
2023-02-03 17:56:18 +0000

    feature(configuration):clarify that bucket index.html is inutil for cloudfront dist. MUST define defaultRootObject for the distn

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,10 +1,11 @@
-import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
+import { Stack, StackProps, CfnOutput, RemovalPolicy } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import {
   aws_s3 as s3,
   aws_s3_deployment as s3deployment,
   aws_cloudfront as cloudfront,
   aws_cloudfront_origins as cloudfront_origins,
+  aws_iam as iam,
 } from "aws-cdk-lib";
 
 export class InfrastructureStack extends Stack {
@@ -14,19 +15,26 @@ export class InfrastructureStack extends Stack {
     const bucket = new s3.Bucket(this, "Bucket", {
       bucketName: "com-bragaboo-friday-bucket",
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+
+      // DEV: Allow bucket removal when Destroy Stack
+      removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
+      autoDeleteObjects: true, // NOT recommended for production code
     });
 
-    const originAccessIdentity = new cloudfront.OriginAccessIdentity(
+    const originAccessId = new cloudfront.OriginAccessIdentity(
       this,
       "OriginAccessIdentity"
     );
 
     const distribution = new cloudfront.Distribution(this, "Distribution", {
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
       defaultRootObject: "index.html",
       defaultBehavior: {
         origin: new cloudfront_origins.S3Origin(bucket, {
-          originAccessIdentity,
+          originAccessIdentity: originAccessId,
         }),
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
     });
 
@@ -40,7 +48,23 @@ export class InfrastructureStack extends Stack {
         distributionPaths: ["/*"],
       }
     );
-    bucket.grantRead(originAccessIdentity);
+    //    bucket.grantRead(originAccessId);
+    // Grant access to cloudfront
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [bucket.arnForObjects("*")],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            originAccessId.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    new CfnOutput(this, "Bucket ARN", {
+      value: bucket.bucketArn,
+    });
 
     new CfnOutput(this, "DistributionId", {
       value: distribution.distributionId,
